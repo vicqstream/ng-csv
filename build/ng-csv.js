85b943e74917f361166a4ee7e333af5992fa65a3
vicqstream <vmuia@qstream.com>
2016-06-23 23:38:35 +0100

    If IE 11 or less download automagically

--- build/ng-csv.js
+++ build/ng-csv.js
@@ -305,7 +305,11 @@ angular.module('ngCsv.directives').
           targetElement.attr('target', '_blank');
 
           if (downloadElement){
-            targetElement.removeClass('ng-hide');
+            if (detectIE() === false) {
+              targetElement.removeClass('ng-hide');
+            }else {
+              window.navigator.msSaveOrOpenBlob(blob, scope.getFilename());
+            }
           } else {
             element.unbind('click');
           }
@@ -317,6 +321,26 @@ angular.module('ngCsv.directives').
           });
           scope.$apply();
         });
+
+        // return IE 11 or less
+        function detectIE() {
+          var ua = window.navigator.userAgent;
+
+          var msie = ua.indexOf('MSIE');
+          if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+          }
+
+          var trident = ua.indexOf('Trident/');
+          if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+          }
+          return false;
+        }
+
       }
     };
   }]);
